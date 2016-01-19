46f0e534df5842196d1cbe396c694c24a834af6c
Huber.Florian <Huber.Florian@192.168.4.35>
2016-01-19 14:57:01 +0100

    initial commit

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Liste liste =new Liste(3);
+	liste.add(5);
+	liste.add(7);
+	
+	System.out.println(liste);
+	
+	
+	
+	}
+
+}
